295f7fd6b41ca30aba1c584aacdccf4627b3a55e
Leonardo <leo.machadop@gmail.com>
2015-03-11 21:54:17 -0300

    Refatoração Ivã

---
+++ src/br/com/pacman/Fantasma.java
@@ -0,0 +1,17 @@
+package br.com.pacman;
+
+public class Fantasma {
+	private char fantasma;
+	
+	public Fantasma() {
+		this.fantasma = '$';
+	}
+	
+	public void setFantasma(char fantasma) {
+		this.fantasma = fantasma;
+	}
+	
+	public char getFantasma() {
+		return this.fantasma;
+	}
+}
--- src/br/com/pacman/Tabuleiro.java
+++ src/br/com/pacman/Tabuleiro.java
@@ -4,29 +4,20 @@ public class Tabuleiro {
 	private char tabuleiro[][];
 	
 	public Tabuleiro() {
-		this.tabuleiro = new char[23][22];
-		for (int i = 0; i < 23; i++) {
-			for(int j = 0; j < 22; j++) {
-				if((i ==1) && (j >= 2) && (j <= 19)) {
-					this.tabuleiro[i][j] = ' ';
-					this.tabuleiro[i][8] = 'P';
-					this.tabuleiro[i][9] = 'A';
-					this.tabuleiro[i][10] = 'C';
-					this.tabuleiro[i][11] = 'M';
-					this.tabuleiro[i][12] = 'A';
-					this.tabuleiro[i][13] = 'N';
-				}
-				else if((i >= 4) && (i <= 18) && (j >= 1) && (j <= 20)) {
-					this.tabuleiro[i][j] = ' ';
-					if((i >= 11) && (i <= 14) && (j >= 5) && (j <= 16)) {
-						this.tabuleiro[i][j] = '*';
-					}
-				}
-				else {
-					this.tabuleiro[i][j] = '*';
-				}
-			}
-		}
+		this.tabuleiro = new char[17][17];
+		for(int i = 0; i < 17; i++) {
+                    for (int j = 0; j < 17; j++) {
+                        if (i == 0 || j == 0) {
+                            tabuleiro[i][j] = '#';
+                        }
+                        else if (i == 16 || j == 16) {
+                            tabuleiro[i][j] = '#';
+                        }
+                        else {
+                            tabuleiro[i][j] = '*';
+                        }
+                    }
+                }
 	}
 	
 	public void setTabuleiro (char[][] tabuleiro) {
@@ -38,8 +29,8 @@ public class Tabuleiro {
 	}
 	
 	public void teste() {
-		for (int i = 0; i < 23; i++) {
-			for(int j = 0; j < 22; j++) {
+		for (int i = 0; i < 17; i++) {
+			for(int j = 0; j < 17; j++) {
 				System.out.print(this.tabuleiro[i][j]);
 			}
 			System.out.print("\n");
